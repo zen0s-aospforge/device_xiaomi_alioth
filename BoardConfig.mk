cbdaf5ecf340d940315e40ea81afdf82e4b0f6d0
Praveen Zalaki <praveenzalaki200@gmail.com>
2025-09-27 19:16:31 +0530

    alioth: Change screen density from 420 to 430

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,7 @@ DEVICE_PATH := device/xiaomi/alioth
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
 # Display
-TARGET_SCREEN_DENSITY := 420
+TARGET_SCREEN_DENSITY := 430
 
 # Init
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_alioth
